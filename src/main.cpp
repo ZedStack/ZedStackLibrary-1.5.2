521306f919537116bf10f118932244715a73044c
ZedStack <agusvali97@gmail.com>
2015-11-22 21:49:28 -0300

    Resolution of the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,5 @@
 #include "../.hpp/ZedStackLibrary.hpp"
 
-<<<<<<< HEAD
-// TODO: Hello.
-// TODO: Other line.
-
-// HACK: Hello there.
-=======
-/**
- * Using git-plus for better performance.
- */
->>>>>>> 006d497a8966d1eb0efb2e9f6625dedcda7efbbc
-
 int main(int argc, char const *argv[]) {
 	std::cout << "Hello World!" << std::endl;
 	return 0;
